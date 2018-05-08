249463cadd9aa2b625e5955b6832af0b42d45e06
Jakob Struye <jakob.struye@uantwerpen.be>
2018-05-08 12:51:48 +0000

    Fixed padding, temporarily replaced regular test with sanity check

--- tcn.py
+++ tcn.py
@@ -6,12 +6,12 @@ import numpy as np
 
 
 def add_temporal_block(inputs, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
-    conv1 = Conv1D(n_outputs, kernel_size, strides=stride, dilation_rate=dilation, padding='same' )(inputs)
+    conv1 = Conv1D(n_outputs, kernel_size, strides=stride, dilation_rate=dilation, padding='causal' )(inputs)
     #weightnorm
     #chomp?
     relu1= Activation('relu')(conv1)
     dropout1 = Dropout(dropout)(relu1)
-    conv2 = Conv1D(n_outputs, kernel_size, strides=stride, dilation_rate=dilation, padding='same' )(dropout1)
+    conv2 = Conv1D(n_outputs, kernel_size, strides=stride, dilation_rate=dilation, padding='causal' )(dropout1)
     #chomp?
     relu2 = Activation('relu')(conv2)
     dropout2 = Dropout(dropout)(relu2)
@@ -26,7 +26,7 @@ def add_temporal_block(inputs, n_inputs, n_outputs, kernel_size, stride, dilatio
 def build_tcn(num_inputs, num_channels, kernel_size=2, dropout=2, seq_len=400) :
     layers = []
     num_levels = len(num_channels)
-    inputs = Input(shape=(seq_len,2))
+    inputs = Input(shape=(seq_len,1))
     current_latest = inputs
     for i in range(num_levels):
         dilation_size = 2 ** i
@@ -52,6 +52,12 @@ def build_data(size, seq_len):
     X = np.reshape(X, (size, seq_len, 2))
     return X, Y
 
+def build_sanity_check_data(size, seq_len):
+    X = np.random.rand(size,seq_len, 1)
+    Y = np.roll(X,-1, axis=1)
+    #Y = np.reshape(Y, (size,seq_len))
+    return X, Y
+
 
 if __name__ == '__main__':
     batch_size = 32
@@ -68,18 +74,21 @@ if __name__ == '__main__':
     input_channels = 2
     n_classes = 1
 
-    train_X, train_Y = build_data(50000,seq_len)
-    test_X, test_Y = build_data(1000, seq_len)
+    #seq_len = 400
+    train_X, train_Y = build_sanity_check_data(50000,seq_len)
+    test_X, test_Y = build_sanity_check_data(1000, seq_len)
 
 
     # Note: We use a very simple setting here (assuming all levels have the same # of channels.
     channel_sizes = [units] * levels
     (ins, outs) = build_tcn(input_channels, channel_sizes, kernel_size=kernel_size, dropout=dropout, seq_len=seq_len)
-    reshape = Reshape((-1,))(outs)
-    linear = Dense(n_classes)(reshape)
-    model = Model(inputs=ins, outputs=linear)
+    #reshape = Reshape((-1,))(outs)
+    linear = Dense(n_classes)(outs)
+    #final = linear
+    final = linear
+    model = Model(inputs=ins, outputs=final)
     aww = AdamWithWeightnorm()
-    model.compile(aww, loss='mse')
+    model.compile(aww, loss='mae')
     data_based_init(model, train_X[:100])
     print(train_Y.shape)
     print(model.summary())
